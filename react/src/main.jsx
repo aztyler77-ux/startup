2ae961c8b6460e9d2fdf283a954afc2ebb6bb7a8
Tyler Nichols <YOUR_BYU_OR_GITHUB_EMAIL_HERE>
2026-02-12 05:01:54 +0000

    style: fix bootstrap override + improve contrast

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,15 +1,16 @@
-import "./startup.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./startup.css";
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
